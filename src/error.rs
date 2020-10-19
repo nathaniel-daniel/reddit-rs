6561f91c343458ce56d59c1dcf83c2a6b39cb4c3
adumbidiot <nathaniel.daniel23@outlook.com>
2020-10-18 18:08:40 -0700

    Add thiserror

--- src/error.rs
+++ src/error.rs
@@ -6,39 +6,32 @@ use http::{
 pub type RedditResult<T> = Result<T, RedditError>;
 
 /// Error type for this library
-#[derive(Debug)]
+#[derive(Debug, thiserror::Error)]
 pub enum RedditError {
     /// Hyper Error
-    Hyper(hyper::Error),
+    #[error(transparent)]
+    Hyper(#[from] hyper::Error),
 
     /// Error parsing a uri
-    InvalidUri(InvalidUri),
+    #[error(transparent)]
+    InvalidUri(#[from] InvalidUri),
 
     /// Invalid status recieved
+    #[error("Invalid Status {0}")]
     InvalidStatus(StatusCode),
 
     /// Failed to find subreddit
+    #[error("Failed to locate subreddit")]
     SubredditNotFound,
 
     /// Error parsing json. The optional bytes object is the json that failed to parse.
-    Json(serde_json::Error, Option<bytes::Bytes>),
+    #[error(transparent)]
+    Json(#[from] serde_json::Error),
 }
 
 impl RedditError {
     /// Returns true if the error type is "SubredditNotFound", false otherwise
     pub fn is_subreddit_not_found(&self) -> bool {
         matches!(self, RedditError::SubredditNotFound)
-    }
-}
-
-impl From<hyper::Error> for RedditError {
-    fn from(e: hyper::Error) -> Self {
-        Self::Hyper(e)
-    }
-}
-
-impl From<InvalidUri> for RedditError {
-    fn from(e: InvalidUri) -> Self {
-        RedditError::InvalidUri(e)
     }
 }
