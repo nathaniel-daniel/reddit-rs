db2530dd48095b43919cad5e163e027f0d21fc91
nathaniel-daniel <nathaniel-daniel@users.noreply.github.com>
2024-10-01 16:56:48 +0000

    deploy: 30e7007c311e7d0751e14c90eddc8caf0dd4dd00

---
+++ src-files.js
@@ -0,0 +1,4 @@
+var srcIndex = new Map(JSON.parse('[\
+["reddit",["",[],["client.rs","error.rs","lib.rs","types.rs"]]]\
+]'));
+createSrcSidebar();
---
+++ trait.impl/core/convert/trait.From.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"reddit":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.81.0/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;Error&gt; for <a class=\"enum\" href=\"reddit/error/enum.Error.html\" title=\"enum reddit::error::Error\">Error</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
